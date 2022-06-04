96083d1853b7db2a9507f1026b626bf7d1c7394f
Michał Staniszewski <stanisz96@gmail.com>
2022-06-04 22:18:43 +0200

    init commit

---
+++ library/const.py
@@ -0,0 +1,5 @@
+import pathlib as pl
+
+MAIN_PATH = pl.Path('C:/Magisterka/data')
+USERS_PATH = pl.Path(MAIN_PATH,"users")
+TWEET_TYPE_NAMES = ["quoted","replied_to","retweeted","tweeted"]
---
+++ main.py
@@ -0,0 +1,19 @@
+import library.const as const
+
+
+
+
+
+
+
+
+
+def main():
+    print("TEST")
+
+
+
+
+
+if __name__=="__main__":
+    main()
